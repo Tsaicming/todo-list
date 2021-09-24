875afa5ed3422539a3e6afb92b2bac75e7223a85
Tsaicming <tsai.cming@gmail.com>
2021-09-24 14:03:14 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.listen(3000, () => {
 })
 
 app.get('/', (req, res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
+  Todo.find()                    // 取出 Todo model 裡的所有資料
+    .lean()                      // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos }))  // 將資料傳給 index 樣板
+    .catch(error => console.error(error))           // 錯誤處理
 })
 
 app.get('/todos/new', (req, res) => {
@@ -37,8 +37,16 @@ app.get('/todos/new', (req, res) => {
 })
 
 app.post('/todos', (req, res) => {
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error)) // 錯誤處理
+  const name = req.body.name          // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })        // 存入資料庫
+    .then(() => res.redirect('/'))        // 新增完成後導回首頁
+    .catch(error => console.log(error))   // 錯誤處理
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)            //從資料庫查找出資料
+    .lean()                           //把資料轉換成單純的 JS 物件
+    .then((todo) => res.render('detail', { todo })) //把資料送給前端樣板
+    .catch(error => console.log(error))             //錯誤處理
 })
