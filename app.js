768e4fb32a113ea7bc7fbf630341e0d3e1b955c9
Tsaicming <tsai.cming@gmail.com>
2021-10-06 13:52:50 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+require('./config/mongoose')
+// Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數
+
 const routes = require('./routes')    // 引用路由器，預設會自動去找指定目錄下的 index 檔案
 const app = express()
-const db = mongoose.connection
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-db.on('error', () => {
-  console.log('mongodb error !')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected !')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
